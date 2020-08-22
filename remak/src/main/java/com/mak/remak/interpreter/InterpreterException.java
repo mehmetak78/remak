45842ee5d0ee0c3b4dbb8a8c3efa5f414720d3c1
mehmetak <mehmetak78@hotmail.com>
2020-08-22 14:07:27 +0300

    fd

--- remak/src/main/java/com/mak/remak/interpreter/InterpreterException.java
+++ remak/src/main/java/com/mak/remak/interpreter/InterpreterException.java
@@ -12,25 +12,8 @@ public class InterpreterException extends Exception {
 		
 	}
 
-	public InterpreterException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
-
-	public InterpreterException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
 	public InterpreterException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
-	}
-
-	public InterpreterException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
